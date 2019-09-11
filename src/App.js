761da8662d05a24f0dea3b423a301456fa5b1602
paulhhowells <p.h.howells@gmail.com>
2019-09-11 10:58:41 +0100

    fix: blurred whole page
    Wrapped page in .page__outline.

--- src/App.js
+++ src/App.js
@@ -8,36 +8,38 @@ import './component/section.css';
 function App () {
 	return (
 		<Page>
-			<main className="page__main baseline">
-				<section className="section">
-					<div className="inner section__inner">
-						<h1>Opening &amp; closing navigation menu</h1>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-							facilisis elit finibus, pharetra sem vitae, accumsan leo. Praesent
-							a dui lectus.
-						</p>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-							facilisis elit finibus, pharetra sem vitae, accumsan leo. Praesent
-							a dui lectus.
-						</p>
-						<ol>
-							<li>A The quick brown fox jumps over the lazy dog</li>
-							<li>B The quick brown fox jumps over the lazy dog</li>
-							<li>C The quick brown fox jumps over the lazy dog</li>
-						</ol>
-						<p>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
-							facilisis elit finibus, pharetra sem vitae, accumsan leo. Praesent
-							a dui lectus.
-						</p>
-					</div>
-				</section>
-			</main>
-			<footer className="footer">
-				<div className="inner footer__inner">Footer</div>
-			</footer>
+			<div className="page__outline">
+				<main className="page__main baseline">
+					<section className="section">
+						<div className="inner section__inner">
+							<h1>Opening &amp; closing navigation menu</h1>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+								facilisis elit finibus, pharetra sem vitae, accumsan leo. Praesent
+								a dui lectus.
+							</p>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+								facilisis elit finibus, pharetra sem vitae, accumsan leo. Praesent
+								a dui lectus.
+							</p>
+							<ol>
+								<li>A The quick brown fox jumps over the lazy dog</li>
+								<li>B The quick brown fox jumps over the lazy dog</li>
+								<li>C The quick brown fox jumps over the lazy dog</li>
+							</ol>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+								facilisis elit finibus, pharetra sem vitae, accumsan leo. Praesent
+								a dui lectus.
+							</p>
+						</div>
+					</section>
+				</main>
+				<footer className="footer">
+					<div className="inner footer__inner">Footer</div>
+				</footer>
+			</div>
 		</Page>
 	);
 }
